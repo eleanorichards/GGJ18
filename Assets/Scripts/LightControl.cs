56cb75f58e530e18a22e604cb5ec4a34c1578db6
Callan Patay <callan2.patay@live.uwe.ac.uk>
2018-01-28 05:25:45 +0000

    led light prefabs

--- Assets/Scripts/LightControl.cs
+++ Assets/Scripts/LightControl.cs
@@ -7,40 +7,72 @@ public class LightControl : MonoBehaviour {
 	[SerializeField] Material lit_mat;
 	[SerializeField] Material unlit_mat;
 
-	[SerializeField] Renderer ren;
+	[SerializeField] GameObject bulb1;
+	[SerializeField] GameObject bulb2;
+
+	[SerializeField] int counter_speed = 200;
+
+	private Renderer ren1;
+	private Renderer ren2;
+
 	private bool lit = false;
 
-	[SerializeField] int counter = 0;
+	private int counter = 0;
+	private bool flicker = true;
 
 	// Use this for initialization
 	void Start () {
-		ren = GetComponent<Renderer> ();
+		ren1 = bulb1.GetComponent<Renderer> ();
+		ren2 = bulb2.GetComponent<Renderer> ();
 
-		ren.material = unlit_mat;
+		ren1.material = unlit_mat;
+		ren2.material = unlit_mat;
+
+		counter_speed = Random.Range (25, 150);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		counter += 1;
-
-		if (counter > 200) 
+		if (flicker) 
 		{
-			counter = 0;
+			counter += 1;
 
-			if (lit)
-			{
-				ren.material = unlit_mat;
-				lit = false;
-			} 
+			if (counter > counter_speed) {
+				counter = 0;
 
-			else 
-			{
-				ren.material = lit_mat;
-				lit = true;
+				if (lit) {
+					ren1.material = unlit_mat;
+					ren2.material = unlit_mat;
+
+					lit = false;
+				} else {
+					ren1.material = lit_mat;
+					ren2.material = lit_mat;
+
+					lit = true;
+				}
 			}
 		}
 		
+	}
+
+	public void TurnOn()
+	{
+		lit = true;
+
+		flicker = true;
+
+		counter = 1;
+	}
+
+	public void TurnOff()
+	{
+		lit = false;
+
+		flicker = false;
+
+		counter = 1;
 	}
 }
